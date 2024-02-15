3fb2fdd124c8eac55495f2e66d92096a03e7dae6
Mehrdad Faghih-Ordobadi <mehrdadfaghih-ordobadi@Mehrdads-MacBook-Pro-2.local>
2024-02-15 15:54:34 -0800

    cleaner tf commands...

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ variable "environment_name" {
   type        = string
 }
 
+variable "backed_key" {
+  description = "s3-table key name"
+  type        = string
+}
+
 # Create your VPC
 resource "aws_vpc" "coderunner_vpc" {
   cidr_block =  "10.0.0.0/16" # CIDR block for the VPC
@@ -15,43 +20,43 @@ resource "aws_vpc" "coderunner_vpc" {
   }
 }   
 
-# Create a internet gateway
-resource "aws_internet_gateway" "cr_igw" {
-  vpc_id = aws_vpc.coderunner_vpc.id
-  tags = {
-    Name = "${var.environment_name}_igw"
-  }
-}
+# # Create a internet gateway
+# resource "aws_internet_gateway" "cr_igw" {
+#   vpc_id = aws_vpc.coderunner_vpc.id
+#   tags = {
+#     Name = "${var.environment_name}_igw"
+#   }
+# }
 
-#  Create a subnet inside your VPC
-resource "aws_subnet" "cr_subnet" {
-  vpc_id = aws_vpc.coderunner_vpc.id
-  cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = true # Enable auto-assign public IP
-  tags = {
-    Name = "${var.environment_name}_subnet"
-  }
-}
+# #  Create a subnet inside your VPC
+# resource "aws_subnet" "cr_subnet" {
+#   vpc_id = aws_vpc.coderunner_vpc.id
+#   cidr_block = "10.0.1.0/24"
+#   map_public_ip_on_launch = true # Enable auto-assign public IP
+#   tags = {
+#     Name = "${var.environment_name}_subnet"
+#   }
+# }
 
-# Create a route table for the VPC
-resource "aws_route_table" "cr_rt1" {
-  vpc_id = aws_vpc.coderunner_vpc.id
+# # Create a route table for the VPC
+# resource "aws_route_table" "cr_rt1" {
+#   vpc_id = aws_vpc.coderunner_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.cr_igw.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.cr_igw.id
+#   }
 
-  tags = {
-    Name = "cr_rt1"
-  }
-}
+#   tags = {
+#     Name = "cr_rt1"
+#   }
+# }
 
-# Associate the route table to the subtnet you created
-resource "aws_route_table_association" "cr_a" {
-  subnet_id = aws_subnet.cr_subnet.id
-  route_table_id = aws_route_table.cr_rt1.id
-}
+# # Associate the route table to the subtnet you created
+# resource "aws_route_table_association" "cr_a" {
+#   subnet_id = aws_subnet.cr_subnet.id
+#   route_table_id = aws_route_table.cr_rt1.id
+# }
 
 
 
@@ -59,6 +64,7 @@ resource "aws_route_table_association" "cr_a" {
   backend "s3" {
     bucket         = "ephemeral-dev-environment-tfstate-bucket"  # Replace with your bucket name
     region         = "us-east-1"                  # Replace with your bucket region
+    key            = var.backed_key
     dynamodb_table = "ephemeral-dev-environment-tfstate-lock-table"              # Replace with your DynamoDB table name
     encrypt        = true
   }
